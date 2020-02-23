a3c2f0a69c908d3bc5c060ed7759c9255a3d03bb
libb <boomlibb1996@gmail.com>
2020-02-23 21:55:53 +0800

    解决bug

--- main.js
+++ main.js
@@ -1,71 +1,51 @@
-let n 
-初始化()
-let timer = setInterval(() => {
-    makeLeave(getImage(n))
-        .one('transitionend', (e) => {
-            makeEnter($(e.currentTarget))
-        })
-    makeCurrent(getImage(n+1))
-    n += 1
-}, 3000)
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
+makeFakeSlides()
+$slides.css({transform:'translateX(-400px)'})
+bindEvents()
 
-document.addEventListener('visibilitychange', function () {
-    console.log(document.hidden)
-    if (document.hidden) {
-        window.clearInterval(timer) 
-    } else {
-       timer =  setInterval(() => {
-            makeLeave(getImage(n))
-                .one('transitionend', (e) => {
-                    makeEnter($(e.currentTarget))
+
+function makeFakeSlides() {
+    let $firstCopy = $images.eq(0).clone(true)
+    let $lastCopy = $images.eq($images.length - 1).clone(true)
+    $slides.append($firstCopy)
+    $slides.prepend($lastCopy)
+}
+    
+function bindEvents() {
+    $buttons.eq(0).on('click', function () {
+        if (current == 2) {
+            $slides.css({ transform: 'translateX(-1600px)' })
+                .one('transitionend', function () {
+                    $slides.hide()
+                    .offset()
+                    $slides.css({ transform: 'translateX(-400px)' })
+                    .show()
+            })
+        } else {
+            $slides.css({ transform: 'translateX(-400px)' })
+        }
+        current = 0
+    })
+    $buttons.eq(1).on('click', function () {
+        
+        $slides.css({ transform: 'translateX(-800px)' })
+        current = 1
+    })
+    $buttons.eq(2).on('click', function () {
+        if (current == 0) {
+            $slides.css({ transform: 'translateX(0px)' })
+                .one('transitionend', function () {
+                    $slides.hide()
+                        .offset()
+                    $slides.css({ transform: 'translateX(-1200px)' })
+                        .show()
                 })
-            makeCurrent(getImage(n+1))
-            n += 1
-        }, 3000)
-    }
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 下面可以不看
-function getImage(n) {
-   return $(`.images>img:nth-child(${x(n)})`)
-}
-
-function x(n) {
-    if (n > 3) {
-        n = n % 3
-        if (n === 0) {
-            n = 3
-        } // n = 1 2 3
-    }
-    return n
-}
-
-function 初始化() {
-    n = 1
-    $(`.images>img:nth-child(${n})`).addClass('current')
-    .siblings().addClass('enter')
-}
-
-function makeCurrent($node) {
-    $node.removeClass('enter leave').addClass('current')
-}
-function makeEnter($node) {
-    $node.removeClass('leave current').addClass('enter')
-}
-function makeLeave($node) {
-    $node.removeClass('enter current').addClass('leave')
-    return $node
+        }else{
+        $slides.css({ transform: 'translateX(-1200px)' })
+        current = 2
+        }
+    })
 }
